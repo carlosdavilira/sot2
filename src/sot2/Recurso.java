589c60bcb41d440eafad9f5c4e6a63ba8dc6ec28
Carlos David <carlosdavilira@gmail.com>
2018-05-28 17:47:34 -0300

    upload sot2

---
+++ src/sot2/Recurso.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sot2;
+
+/**
+ *
+ * @author Carlos David
+ */
+public class Recurso {
+
+private String nomeRecurso;
+private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getNomeRecurso() {
+        return nomeRecurso;
+    }
+
+    public void setNomeRecurso(String nomeRecurso) {
+        this.nomeRecurso = nomeRecurso;
+    }
+
+
+
+    
+}
